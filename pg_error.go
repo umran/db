95930afacecedf8551fd8daae3c26aab6a575cac
Umran Hussein <umran.hussein@gmail.com>
2020-08-25 19:39:58 -0700

    removed check for crdb and pgx error cause

--- pg_error.go
+++ pg_error.go
@@ -23,37 +23,16 @@ func errorCause(err error) error {
 }
 
 func errIsRetryable(err error) bool {
-	// We look for either:
-	//  - the standard PG errcode SerializationFailureError:40001 or
-	//  - the Cockroach extension errcode RetriableError:CR000. This extension
-	//    has been removed server-side, but support for it has been left here for
-	//    now to maintain backwards compatibility.
 	code := errCode(err)
 
-	// debugging
-	if !(code == "CR000" || code == "40001") {
-		// fmt.Println(errorCause(err))
-	}
-
-	return code == "CR000" || code == "40001"
+	return code == "40001"
 }
 
 func errCode(err error) string {
 	switch t := errorCause(err).(type) {
 	case *pq.Error:
 		return string(t.Code)
-
-	case errWithSQLState:
-		return t.SQLState()
-
 	default:
 		return ""
 	}
-}
-
-// errWithSQLState is implemented by pgx (pgconn.PgError).
-//
-// TODO(andrei): Add this method to pq.Error and stop depending on lib/pq.
-type errWithSQLState interface {
-	SQLState() string
 }
